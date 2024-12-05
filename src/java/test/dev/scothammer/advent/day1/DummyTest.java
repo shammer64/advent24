fb3de4ca372d5496202e5ec5c0709ff6ea37133c
Scott Hammer <scott.hammer@thehartford.com>
2024-12-05 11:42:42 -0500

    Starting point with working test

---
+++ src/java/test/dev/scothammer/advent/day1/DummyTest.java
@@ -0,0 +1,14 @@
+package dev.scothammer.advent.day1;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
