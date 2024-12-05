f37417f046375765f5f1b3f8c33eb9eb426a424b
Scott Hammer <scott.hammer@thehartford.com>
2024-12-05 12:09:21 -0500

    Implemented processing of single item lists; exception for lists of diff length

--- src/java/test/dev/scothammer/advent/day1/Day1Test.java
+++ src/java/test/dev/scothammer/advent/day1/Day1Test.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Day1Test {
 
@@ -15,8 +16,38 @@ public class Day1Test {
         assertEquals(0, sum);
     }
 
+    @Test
+    public void givenTwoSingleItemLists_sumOfDiffsIsDiffOfItems() {
+        long[] list1 = {15L};
+        long[] list2 = {8L};
+        long sum = Day1.sumOfDiffs(list1, list2);
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void givenTwoSingleItemLists_sumOfDiffsIsPositiveDiffOfItems() {
+        long[] list1 = {8L};
+        long[] list2 = {15L};
+        long sum = Day1.sumOfDiffs(list1, list2);
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void givenTwoListsOfDiffLength_throwException() {
+        long[] list1 = {8L, 19L};
+        long[] list2 = {15L};
+        assertThrows(IllegalArgumentException.class,
+                () -> Day1.sumOfDiffs(list1, list2));
+    }
+
     private static class Day1 {
-        public static int sumOfDiffs(long[] list1, long[] list2) {
+        public static long sumOfDiffs(long[] list1, long[] list2) {
+            if (list1.length != list2.length) {
+                throw new IllegalArgumentException("Lists must be of equal length");
+            }
+            if (list1.length > 0) {
+                return Math.abs(list1[0] - list2[0]);
+            }
             return 0;
         }
     }
