dce473a49a2c5c756d7528bfc937eec263f5711e
Scott Hammer <scott.hammer@thehartford.com>
2024-12-05 15:44:36 -0500

    Implemented test against sample data; corrected logic error; implemented final test against day1_input.txt

--- src/test/java/day1/Day1Test.java
+++ src/test/java/day1/Day1Test.java
@@ -1,9 +1,12 @@
-package dev.scothammer.advent.day1;
+package day1;
 
 import org.junit.jupiter.api.Test;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -43,6 +46,7 @@ public class Day1Test {
     }
 
     @Test
+    @SuppressWarnings("nullness")
     public void givenOneNullList_throwAssertionError() {
         long[] list1 = null;
         long[] list2 = {15L};
@@ -55,7 +59,7 @@ public class Day1Test {
         long[] list1 = {15L, 21L};
         long[] list2 = {8L, 17L};
         long sum = Day1.sumOfDiffs(list1, list2);
-        assertEquals(15, sum);
+        assertEquals(11, sum);
     }
 
     @Test
@@ -63,16 +67,39 @@ public class Day1Test {
         long[] list1 = {21L, 15L};
         long[] list2 = {8L, 17L};
         long sum = Day1.sumOfDiffs(list1, list2);
-        assertEquals(15, sum);
+        assertEquals(11, sum);
     }
 
-//     Sample Input
-//        3   4
-//        4   3
-//        2   5
-//        1   3
-//        3   9
-//        3   3
+    @Test
+    public void givenSampleData_sumOfDiffsIsEleven() {
+        long[] list1 = {3, 4, 2, 1, 3, 3};
+        long[] list2 = {4, 3, 5, 3, 9, 3};
+        long sum = Day1.sumOfDiffs(list1, list2);
+        assertEquals(11, sum);
+    }
+
+    @Test
+    public void givenDay1Input_computeAnswer() {
+        URL inputFileURL = ClassLoader.getSystemResource("day1_input.txt");
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFileURL.getFile()))) {
+            List<String[]> values = fileReader.lines()
+                    .map(line -> line.split("\\s+"))
+                    .toList();
+            long[] list1 = values.stream()
+                    .map(arr -> Long.parseLong(arr[0]))
+                    .mapToLong(Long::longValue)
+                    .toArray();
+            long[] list2 = values.stream().map(arr -> Long.parseLong(arr[arr.length - 1]))
+                    .mapToLong(Long::longValue)
+                    .toArray();
+            long sum = Day1.sumOfDiffs(list1, list2);
+            assertEquals(1388114, sum);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private static class Day1 {
         public static long sumOfDiffs(long[] list1, long[] list2) {
@@ -82,7 +109,7 @@ public class Day1Test {
             Arrays.sort(list1);
             Arrays.sort(list2);
             for (int i = 0; i < list1.length; i++) {
-                sum += Math.abs(list1[i] - list2[list2.length - 1 - i]);
+                sum += Math.abs(list1[i] - list2[i]);
             }
             return sum;
         }
